f21a65360f324e0609b1988234afadf1a588c40b
Alberto Guerrero <albergr1197@gmail.com>
2020-08-24 22:09:44 +0200

    mejoras

--- src/main/java/acme/features/administrator/bookkeeperRequester/AdministratorBookkeeperRequesterRepository.java
+++ src/main/java/acme/features/administrator/bookkeeperRequester/AdministratorBookkeeperRequesterRepository.java
@@ -7,14 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.roles.BookkeeperRequester;
-import acme.framework.entities.UserAccount;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
 public interface AdministratorBookkeeperRequesterRepository extends AbstractRepository {
-
-	@Query("select ua from UserAccount ua where ua.id=?1")
-	UserAccount findOneUserAccountById(int id);
 
 	@Query("select br from BookkeeperRequester br")
 	Collection<BookkeeperRequester> findManyBookkeeperRequester();
